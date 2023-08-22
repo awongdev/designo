3654ed237eb90b46b52a93321f70d8cad411c3ae
Aaron W <26586148+AceDub@users.noreply.github.com>
2023-08-22 13:34:09 -0700

    Fix: Inconsistent sizes of cards on certain screen sizes

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -92,7 +92,7 @@ const projects = {
 const Card = ({ project }: ProjectT) => {
   const currentProject = projects[project];
   return (
-    <div className="group grid w-full max-w-1110 cursor-pointer overflow-hidden rounded-2xl sm:grid-flow-col sm:grid-cols-2 lg:grid-flow-row lg:grid-cols-none">
+    <div className="group flex cursor-pointer flex-col overflow-hidden rounded-2xl md:flex-row lg:flex-col">
       <Image
         src={currentProject.img}
         alt={currentProject.alt}
@@ -100,7 +100,7 @@ const Card = ({ project }: ProjectT) => {
         height={620}
         className="h-full w-full"
       />
-      <div className="flex w-full flex-col items-center justify-center bg-[#FDF3F0] px-[1.875rem] py-8 text-center transition-colors duration-300 group-hover:bg-clr-peach">
+      <div className="flex h-full w-full flex-col items-center justify-start bg-[#FDF3F0] px-[1.875rem] py-8 text-center transition-colors duration-300 group-hover:bg-clr-peach md:justify-center lg:justify-start">
         <h2 className="mb-4 text-[1.25rem] font-medium uppercase tracking-[5px] text-clr-peach group-hover:text-clr-white">
           {currentProject.title}
         </h2>
